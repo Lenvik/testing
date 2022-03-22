fd540a26445e1bbb416af610d6652314b108ae28
Oroshan <72888510+OroshanH@users.noreply.github.com>
2022-03-22 17:32:22 +0100

    Ny kode

--- src/test/java/oslomet/testing/SikkerhetsController.java
+++ src/test/java/oslomet/testing/SikkerhetsController.java
@@ -13,9 +13,12 @@ import org.springframework.mock.web.MockHttpSession;
 import oslomet.testing.DAL.BankRepository;
 import oslomet.testing.Sikkerhet.Sikkerhet;
 
+import javax.servlet.http.HttpSession;
 import java.util.HashMap;
 import java.util.Map;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
@@ -31,7 +34,7 @@ public class SikkerhetsController {
     private BankRepository repository;
 
     @Mock
-    private MockHttpSession session;
+    MockHttpSession session;
 
     @Before
 
@@ -57,14 +60,54 @@ public class SikkerhetsController {
         }).when(session).setAttribute(anyString(), any());
     }
 
-
+    @Test
+    public void sjekkLogInn_OK(){
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("OK");
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","Passord");
+        assertEquals("OK",resultat);
+    }
 
     @Test
-    public void sjekkLogin_ok(){
-
+    public void sjekkLogInn_IkkeOkEn(){
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i personnummer");
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","Passord");
+        assertEquals("Feil i personnummer eller passord",resultat);
+    }
+    @Test
+    public void sjekkLogInn_IkkeOkTo(){
+        when(repository.sjekkLoggInn(anyString(),anyString())).thenReturn("Feil i passord");
+        String resultat = sikkerhetsController.sjekkLoggInn("12345678901","Passord");
+        assertEquals("Feil i personnummer eller passord",resultat);
     }
 
 
+    @Test
+    public void sjekkLogInnAdmin_Ok(){
+        String resultat = sikkerhetsController.loggInnAdmin("Admin","Admin");
+        assertEquals("Logget inn",resultat);
+    }
+   @Test
+    public void sjekkLogInnAdmin_IkkeOk(){
+        session.setAttribute("Innlogget",null);
+        String resultat = sikkerhetsController.loggInnAdmin("","");
+        assertEquals("Ikke logget inn",resultat);
+    }
+
+
+
+    @Test
+    public void sjekkLoggetInn_OK(){
+        session.setAttribute("Innlogget","12345678901");
+        String resultat = sikkerhetsController.loggetInn();
+        assertEquals("12345678901",resultat);
+    }
+
+    @Test
+    public void sjekkLoggetInn_IkkeOK(){
+        session.setAttribute("Innlogget",null);
+        String resultat = sikkerhetsController.loggetInn();
+        assertNull(resultat);
+    }
 
 }
 
